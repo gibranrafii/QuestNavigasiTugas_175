e9f4e65f9fa4ce98d96e783d8ec43f990bf63504
gibranrafii <gibranrafi21@gmail.com>
2025-11-08 18:05:56 +0700

    membuat input dan textfield untuk nama

--- app/src/main/java/com/example/tugasprak5/view/Tampilan3.kt
+++ app/src/main/java/com/example/tugasprak5/view/Tampilan3.kt
@@ -3,13 +3,25 @@ package com.example.tugasprak5.view
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -19,8 +31,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tugasprak5.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -75,7 +90,23 @@ fun formPendaftaran (
                     containerColor = colorResource(id = R.color.biru)
                 )
             ){
+                Text(
+                    stringResource(id = R.string.nama_lengkap),
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(top = 12.dp, start = 16.dp, bottom = 10.dp)
+                )
+                OutlinedTextField(
+                    value = textNama,
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, end = 12.dp),
+                    label = { Text(text = "Isian Nama Lengkap") },
 
+                    onValueChange = {
+                        textNama = it
+                    }
+                )
             }
         }
     }
