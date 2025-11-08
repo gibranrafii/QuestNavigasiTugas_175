9aa3bdba75a08262cb8fe971a3af30b3cd9a76d2
gibranrafii <gibranrafi21@gmail.com>
2025-11-08 17:57:29 +0700

    membuat fungsi formPendaftaran

--- app/src/main/java/com/example/tugasprak5/view/Tampilan3.kt
+++ app/src/main/java/com/example/tugasprak5/view/Tampilan3.kt
@@ -1,2 +1,31 @@
 package com.example.tugasprak5.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.tugasprak5.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun formPendaftaran (
+    OnKembaliBtnClick: () -> Unit,
+    OnSubmitBtnClick: () -> Unit
+) {
+
+}
