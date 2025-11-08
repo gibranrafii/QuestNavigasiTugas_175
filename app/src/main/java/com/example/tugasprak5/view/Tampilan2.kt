a9a742ca27433b2c19042501a5c87caf33cd079e
gibranrafii <gibranrafi21@gmail.com>
2025-11-08 17:24:53 +0700

    membuat funsgi listForm sebagai fungsi untuk menampilkan list list data dummy

--- app/src/main/java/com/example/tugasprak5/view/Tampilan2.kt
+++ app/src/main/java/com/example/tugasprak5/view/Tampilan2.kt
@@ -1,2 +1,13 @@
 package com.example.tugasprak5.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun listForm (
+    OnBackBtnClick : () -> Unit,
+    OnNextBtnClick : () -> Unit,
+) {
+
+}
